81eeff4d49bb7f80e78d851634aebd7e0ce46e2a
Cai <Cai.Williams@durham.ac.uk>
2020-06-16 14:06:12 +0100

    a

--- Init.py
+++ Init.py
@@ -2,5 +2,44 @@ import numpy as np
 import os
 import h5py
 
-def Gen():
+def CreateProject():
+    ProName = ProjectName()
+    ProLoc, Lat, Lon = ProjectLoc()
     
+def ProjectName():
+    os.system('cls')
+    print("What is the name of the project?")
+    name = input()
+    print("Is " + name + "correct? Y/N")
+    X = input().lower()
+    if X == "y":
+        return name
+    elif X == "n":
+        ProjectName()
+    else:
+        print("Invalid Entry!")
+        ProjectName()
+    return
+
+def ProjectLoc():
+    os.system('cls')
+    print("Location Name:")
+    name = input()
+    print("Location Latitude:")
+    Lat = float(input())
+    print("Location Longitude:")
+    Lon = float(input())
+    os.system('cls')
+    print("Location Name: " + name)
+    print("Location Latitude: " + Lat)
+    print("Location Longitude: "+ Lon)
+    print("Location Details Correct? Y/N")
+    X = input().lower()
+    if X == "y":
+        return name,Lat,Lon 
+    elif X == "n":
+        ProjectLoc()
+    else:
+        print("Invalid Entry!")
+        ProjectLoc()
+    return
